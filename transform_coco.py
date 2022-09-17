a2d5ab00ff502e281ffabfb732b8205dc50f5320
Anant Gupta <anantgupta129@gmail.com>
2022-09-17 17:05:47 +0530

    code formating

--- transform_coco.py
+++ transform_coco.py
@@ -7,7 +7,7 @@ import json
 import os
 
 
-path_to_lab = r'data\coco_val2017\panoptic_val2017.json'
+path_to_lab = r"data\coco_val2017\panoptic_val2017.json"
 coco = json.load(open(path_to_lab))
 base_path = "data/coco_val2017/images"
 
@@ -17,7 +17,7 @@ id_2_category = {}
 category_2_id = {}
 category_id = 0
 
-for category in coco['categories']:
+for category in coco["categories"]:
     if category["isthing"]:
         things.append(category["id"])
     else:
@@ -28,46 +28,46 @@ for category in coco['categories']:
         category_id += 1
 
 updated_categories.append(
-    {    
+    {
         "supercategory": "",
         "isthing": 0,
         "id": category_id,
-        "name": "miscellaneous stuff"
+        "name": "miscellaneous stuff",
     }
 )
-miscellaneous_id = category_id+1
+miscellaneous_id = category_id + 1
 
-updated_images = [] 
+updated_images = []
 updated_anno = []
 for i in range(1000):
-    image = coco['images'][i]
+    image = coco["images"][i]
     image_id = image["id"]
     image["file_name"] = "{}/{}".format(base_path, image["file_name"])
 
-    for lab in coco['annotations']:
-        if image_id==lab['image_id']:
+    for lab in coco["annotations"]:
+        if image_id == lab["image_id"]:
             segments_info = lab["segments_info"]
             for info in segments_info:
                 if info["category_id"] in things:
                     info["category_id"] = miscellaneous_id
                 else:
-                    info["category_id"] = category_2_id[id_2_category[info["category_id"]]]
-            lab['file_name'] = "{}/{}".format(base_path, lab["file_name"])
+                    info["category_id"] = category_2_id[
+                        id_2_category[info["category_id"]]
+                    ]
+            lab["file_name"] = "{}/{}".format(base_path, lab["file_name"])
 
             updated_images.append(image)
             updated_anno.append(lab)
             break
-    
+
 
 res_file = {
     "info": coco["info"],
     "licenses": coco["licenses"],
     "images": updated_images,
     "annotations": updated_anno,
-    "categories": updated_categories
+    "categories": updated_categories,
 }
 
 with open("data/coco_val2017/coco.json", "w") as f:
     f.write(json.dumps(res_file))
-
-
